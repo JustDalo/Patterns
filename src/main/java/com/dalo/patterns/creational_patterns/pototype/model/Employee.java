db712c11703e7c2180a78f04f400339dc1d44cdf
JustDalo <shishlodan@mail.ru>
2022-04-10 16:11:15 +0300

    prototype pattern

---
+++ src/main/java/com/dalo/patterns/creational_patterns/pototype/model/Employee.java
@@ -0,0 +1,20 @@
+package com.dalo.patterns.creational_patterns.pototype.model;
+
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Employee() {
+
+    }
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Employee copy() {
+        Employee employeeClone = new Employee(this.firstName, this.lastName);
+        return employeeClone;
+    }
+}
