cf0a9f4041d99a231e6f2b6954e9ec6f71ce16f3
JustDalo <shishlodan@mail.ru>
2022-04-11 08:30:53 +0300

    observer

--- src/main/java/com/dalo/patterns/behavioral_patterns/iterator/iterator/impl/EntityCollectionImpl.java
+++ src/main/java/com/dalo/patterns/behavioral_patterns/iterator/iterator/impl/EntityCollectionImpl.java
@@ -3,11 +3,13 @@ package com.dalo.patterns.behavioral_patterns.iterator.iterator.impl;
 import com.dalo.patterns.behavioral_patterns.iterator.iterator.EntityCollection;
 import com.dalo.patterns.behavioral_patterns.iterator.iterator.EntityIterator;
 
+import java.util.ArrayList;
+
 public class EntityCollectionImpl implements EntityCollection {
     EntityIterator entityIterator;
     @Override
     public EntityIterator getIterator() {
-        entityIterator = new EntityIteratorImpl();
+        entityIterator = new EntityIteratorImpl(new ArrayList<>());
         return entityIterator;
     }
 }
---
+++ src/main/java/com/dalo/patterns/behavioral_patterns/observer/event/EventListener.java
@@ -0,0 +1,5 @@
+package com.dalo.patterns.behavioral_patterns.observer.event;
+
+public interface EventListener {
+    void update(String eventType);
+}
